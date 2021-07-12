5b41c6bee40d802167d0c6aa78fe1fcb1c660ce7
freieslebenadam <adam.fre@seznam.cz>
2021-07-12 20:26:02 +0200

    favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Favicon from 'react-favicon'
 import './App.scss'
 import Header from './components/Header'
 import Intro from './components/Intro'
@@ -6,18 +7,22 @@ import Shortener from './components/Shortener'
 import Statistics from './components/Statistics'
 import Footer from './components/Footer'
 import {ShortContextProvider} from './ShortContext'
+import fav from './images/favicon_32x32.png'
 
 function App() {
     return (
-        <ShortContextProvider>
-            <Header />
-            <main>
-                <Intro />
-                <Shortener />
-                <Statistics />
-            </main>
-            <Footer />
-        </ShortContextProvider>
+        <>
+            <Favicon url={fav} />
+            <ShortContextProvider>
+                <Header />
+                <main>
+                    <Intro />
+                    <Shortener />
+                    <Statistics />
+                </main>
+                <Footer />
+            </ShortContextProvider>
+        </>
     )
 }
 
